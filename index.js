ab03abe2bfdc65ddba897b90df584a542690fabc
Büşra <58669314+eminebusrasalihoglu@users.noreply.github.com>
2022-11-02 11:27:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let myData = new Date();
 console.log('UTC ' + myData);
-let trDate = myDate.toLocaleString({ timeZone: 'Turkey/Istanbul' });
+let trDate = myDate.toLocaleString({ timeZone: 'Europe/Istanbul' });
 console.log('your time zone ' + trDate);
